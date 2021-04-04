b54a96cc33f034806c0d0e9345930d7be887aa30
TheStrikeM <dinkodanil@gmail.com>
2021-04-04 07:08:03 +0300

    initial commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,30 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
+import { Module, OnApplicationBootstrap } from '@nestjs/common';
 import { AppService } from './app.service';
+import {RedisModule, RedisService} from "nestjs-redis";
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [
+      RedisModule.register({
+        url: 'redis://localhost:10000/1'
+      })
+  ],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements OnApplicationBootstrap {
+    constructor(
+        private readonly redisService: RedisService
+    ) {
+    }
+
+    public onApplicationBootstrap(): void {
+        let i = 0
+        setInterval(() => {
+            i++
+            const client = this.redisService.getClient()
+            client.rpush("queue:mail.send", JSON.stringify({
+                email: 'dinkodanil@gmail.com',
+                message: `Hello world #${i}`
+            }))
+        }, 1000)
+    }
+}
